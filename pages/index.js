8a96aab1bab6c49ff56bbdc790c338a94e05ab9b
artur.rusak <artur.rusak@instinctools.ru>
2019-11-25 18:14:11 +0300

    add d3

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import React from "react";
+import * as d3 from "d3";
+import css from "styled-jsx/css";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const styles = css`
+  .content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: #dadada;
+    justify-items: center;
+    align-items: center;
+  }
+  .content-item.graph {
+    grid-column-start: span 2;
+    padding: 1em;
+  }
+`;
 
 export default function Home() {
-  return <div>
-    <Header/>
-    <h1>Hello World !!!!!</h1>
-    <Footer/>
-  </div>
+  const linearScale = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([0, 1]);
+
+  console.log(linearScale);
+
+  return (
+    <div>
+      <Header />
+      <div className="content">
+        <div className="content-item">
+          <h1>Hello World !!!!!</h1>
+        </div>
+        <div className="content-item">
+          <span>RIGHT</span>
+        </div>
+        <div className="content-item graph">
+          <span>Graph</span>
+        </div>
+      </div>
+      <Footer />
+      <style jsx>{styles}</style>
+    </div>
+  );
 }
