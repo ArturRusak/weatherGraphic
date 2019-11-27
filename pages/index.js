1ee09e707b7232dd4f37af73c647822464cbed79
artur.rusak <artur.rusak@instinctools.ru>
2019-11-27 17:53:55 +0300

    play with d3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
 import css from "styled-jsx/css";
+import fetch from "isomorphic-unfetch";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -19,12 +20,28 @@ const styles = css`
   }
 `;
 
-export default function Home() {
+export default function Home(props) {
   useEffect(() => {
-    const data = [-19, 12, 15, 65, 43, 88, 96, 34];
+    const { data, error } = props;
+    let dateMarkers = {
+      date: [],
+      time: []
+    };
+    if (data) {
+      data.hourly.data.map(item => {
+        const timeInSeconds = item.time * 1000;
+        const date = new Date(timeInSeconds);
 
+        item.time = date;
+        dateMarkers.date.push(item.time);
+        dateMarkers.time.push(date.getHours());
+      });
+    }
+
+    console.log(data, "----");
+    console.log(dateMarkers.date, "----");
     const margin = { top: 10, right: 30, bottom: 90, left: 40 };
-    const width = 460 - margin.left - margin.right;
+    const width = 960 - margin.left - margin.right;
     const height = 450 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
@@ -39,21 +56,24 @@ export default function Home() {
     // Add Y axis
     const y = d3
       .scaleLinear()
-      .domain([d3.min(data), d3.max(data)])
+      .domain([d3.min(dateMarkers.time), d3.max(dateMarkers.time)])
       .range([400, 0]);
 
     const bottom = d3
-      .scaleLinear()
-      .domain([d3.min(data), d3.max(data)])
-      .range([0, 400]);
+      .scaleTime()
+      .domain([
+        dateMarkers.date[0],
+        dateMarkers.date[dateMarkers.date.length - 1]
+      ])
+      .range([0, 960]);
 
     svg.append("g").call(d3.axisLeft(y));
 
     svg
       .append("g")
       // eslint-disable-next-line no-undef
-      .attr("transform", `translate(-66, ${height - 16})`)
-      .call(d3.axisBottom(bottom));
+      .attr("transform", `translate(0, 400)`)
+      .call(d3.axisBottom(bottom).ticks(40));
   }, []);
 
   return (
@@ -75,3 +95,19 @@ export default function Home() {
     </div>
   );
 }
+
+Home.getInitialProps = async function() {
+  // eslint-disable-next-line no-undef
+  const response = await (() =>
+    fetch(
+      "https://api.darksky.net/forecast/177ed2d5a571643fc368b4f16acc1802/53.54,27.30?lang=ru&exclude=flags,alerts"
+    )
+      .then(res => res.json())
+      .then(data => {
+        return { data };
+      })
+      .catch(error => {
+        return { error };
+      }))();
+  return response;
+};
